44e4459de2a0793b2ba2e2538f5fc0f3ef3eb8e0
4lx9ri4 <tuil_alexandre@hotmail.com>
2017-02-01 11:30:57 +0100

    boutons retirés

--- js/custom.js
+++ js/custom.js
@@ -4,40 +4,40 @@ $(document).ready(function(){
     $('nav').toggleClass('isOpen');
   });
 
-});
-
-/*=====================*/
-/*   TOGGLE BISTROT  & MAP (Iframe)  */
-/*=====================*/
-$(document).ready(function(){
-  $('#imgtarifs1').hide(); //Boutons tarifs en cour
+  /*=====================*/
+  /*   TOGGLE BISTROT  & MAP (Iframe)  */
+  /*=====================*/
   $('#mainbistrot').hide();
   $('iframe').hide();
-});
-  $(function(){
-    $('#boutonbistrot').click(function(){
-      $('#mainbistrot').toggle('blind');
-    });
-    $('.rue_footer').click(function(){
-      $('iframe').toggle('clip', 'slow');
-      return false;
-    });
 
-    //Slider d'images Section 4
-    $(function(){
-      setInterval(function(){
-         $(".slideshow .ulslider").animate({marginLeft:-1000},800,function(){
-            $(this).css({marginLeft:-0}).find("li:last").after($(this).find("li:first"));
-         })
-      }, 3500);
-   });
+  /*===BISTROT===*/
+  $('#boutonbistrot').click(function(){
+    $('#mainbistrot').toggle('blind');
   });
-
-  /*========================*/
-  /*   FERMETURE BISTROT    */
-  /*========================*/
-$(function(){
   $('#fermer').click(function(){
     $('#mainbistrot').hide('blind');
+  });
+  /*===Iframe===*/
+  $('.rue_footer').click(function(){
+    $('iframe').toggle('clip', 'slow');
+    return false;
+  });
+
+  /*===================*/
+  /*  Slider section4  */
+  /*===================*/
+  setInterval(function(){
+     $(".slideshow .ulslider").animate({marginLeft:-1000},800,function(){
+        $(this).css({marginLeft:-0}).find("li:last").after($(this).find("li:first"));
+     })
+  }, 3500);
+
+  /*===================*/
+  /*   Hover Tarifs    */
+  /*===================*/
+  $('#hovertarifs').hover(function(){
+    $('li.tarif').css({
+      'background-color': 'rgba(209, 172, 53, 0.8);'
+    });
   });
 });
